eab2b9e33e2758e2bdc01d71ec9efd9dd08ad938
Marco Aurélio Penha <marcopenha.dev@gmail.com>
2024-12-10 17:05:13 -0300

    Atividade Typescript EBAC

---
+++ src/pratique.ts
@@ -0,0 +1,10 @@
+function multiplicaNumeros(x:number, y:number): number{
+    return x * y;
+}
+const resultadoMultiplicacao = console.log (multiplicaNumeros(5, 10));
+
+
+function saudacao(nome:string): void{
+    console.log("Olá " + nome + "!");
+}
+const resultadoSaudacao = saudacao("Marco");
